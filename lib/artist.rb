b14649d26803e0c8a12828bee2d3d4176a39a55c
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 16:09:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,16 +11,13 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@artists
   end
   
   extend Memorable #for count & reset_all methods
   extend Findable
+  
   def add_song(song)
     @songs << song
     song.artist = self
